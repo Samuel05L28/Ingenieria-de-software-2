795813db758f5d4fafc0b6167cf00628902a3acc
Samuel05L28 <124807214+Samuel05L28@users.noreply.github.com>
2024-10-06 21:21:24 -0500

    Delete POLIMORFISMO directory

--- POLIMORFISMO/Transporte.java
+++
@@ -1,5 +0,0 @@
-public interface Transporte {
-    void acelerar();
-    void frenar();
-    void mostrarInformacion();
-}
--- POLIMORFISMO/garaje.java
+++
@@ -1,6 +0,0 @@
-public class Garaje {
-    public void estacionar(Transporte transporte) {
-        System.out.println("Estacionando el transporte...");
-        transporte.mostrarInformacion();
-    }
-}
